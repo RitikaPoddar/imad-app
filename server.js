92772a160e3fc48b10987a9d812f99d839de1887
RitikaPoddar <30748677+RitikaPoddar@users.noreply.github.com>
2017-08-20 18:25:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool= require('pg').pool();
+var Pool= require('pg').pool;
 var config={
     user:'ritikapoddar1991',
     database:'ritikapoddar1991',
